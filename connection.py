8de8f8c833b81939a07a762ad95e61ef979ea356
YoniVolind <yvolind@gmail.com>
2024-04-29 01:28:26 +0300

    updated ttl and refresh button

--- connection.py
+++ connection.py
@@ -2,13 +2,18 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import time
 
-st.title("יורו 2024")
+st.title("יורו 2024 - פלוגה ב', 8132")
+
+def clear_my_cache():
+    st.cache_data.clear()
+
+st.button('Refresh!', on_click=clear_my_cache,use_container_width=True)
 
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 ttl_seconds = 10
-data = conn.read(worksheet='Games', ttl=ttl_seconds, usecols=list(range(6)))
-st.dataframe(data)
+# data = conn.read(worksheet='Games', ttl=ttl_seconds, usecols=list(range(6)))
+# st.dataframe(data)
 
 sql='''
 select "Game Number","Side a","Side b", "Score a", "Score b"
@@ -18,7 +23,7 @@ where
 "Game Number" is not null
 '''
 df1=conn.query(sql=sql, ttl=ttl_seconds)
-st.dataframe(df1, height=700,width=500)
+st.dataframe(df1, height=700,width=500,hide_index=True)
 
 sql1='''
 select "rank","name","points"
@@ -31,10 +36,8 @@ order by "rank"
 
 '''
 df=conn.query(sql=sql1)
-st.dataframe(df)
-def clear_my_cache():
-    st.cache_data.clear()
+st.dataframe(df,hide_index=True)
 
-st.button('Clear memory', on_click=clear_my_cache)
+
 
 
